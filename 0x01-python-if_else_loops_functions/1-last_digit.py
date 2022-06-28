1dadf183fbed55001fcecf9460e3810e2be41b7d
Ololade <lolareliza@gmail.com>
2022-06-28 13:40:17 -0700

    Last digit update

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,5 +7,5 @@ if digit > 5:
     print(f"{varStr} {number:d} is {digit:d} and is greater than 5")
 elif digit == 0:
     print(f"{varStr} {number:d} is {digit:d} and is 0")
-elif digit < 0 and digit != 0:
-    print(f"{varStr} {number:d} is {digit:d} and is less than 0")
+elif digit < 6 and digit != 0
+    print(f"{varStr} {number:d} is {digit:d} and is less than 6 and not 0")
