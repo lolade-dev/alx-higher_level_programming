f4de74636ce7f27bc562d8d92fe1ecbfbac11b59
Ololade <lolareliza@gmail.com>
2022-07-04 00:47:15 -0700

    Refactor codes

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    nargs = len(sys.argv) - 1
-    if (nargs == 0):
-        print("{:d} arguments.".format(nargs))
-    elif (nargs == 1):
-        print("{:d} argument:".format(nargs))
-    else:
-        print("{:d} arguments:".format(nargs))
-    for str in range(len(sys.argv)):
+    from sys import argv
+    nargs = len(argv)
+    print("{:d} {:s}{:s}".format(nargs - 1, "argument" if nargs <= 2 else "arguments", "." if nargs == 1 else ":"))
+    for str in range(len(argv)):
         if (str == 0):
             continue
-        print("{:d}: {:s}".format(str, sys.argv[str]))
+        print("{:d}: {:s}".format(str, argv[str]))
