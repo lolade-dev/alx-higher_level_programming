e263a57c9a46e313df56a04f24e93af85187b359
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-07-04 08:49:49 +0100

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,8 +2,8 @@
 if __name__ == "__main__":
     from sys import argv
     nargs = len(argv)
-    print("{:d} {:s}{:s}".format(nargs - 1, "argument" if nargs <= 2 else "arguments", "." if nargs == 1 else ":"))
-    for str in range(len(argv)):
-        if (str == 0):
-            continue
-        print("{:d}: {:s}".format(str, argv[str]))
+    print("{:d} {:s}{:s}".format(nargs - 1, "argument" if nargs <= 2 else "arguments",
+                                 "." if nargs == 1 else ":"))
+    for i, s in enumerate(argv):
+        if i > 0:
+            print("{:d}: {:s}".format(i, s))
