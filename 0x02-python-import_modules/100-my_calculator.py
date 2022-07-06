a60e6e2419d78f7e91433942271b2df0531dff20
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-07-06 08:11:05 +0100

    Update 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -20,5 +20,5 @@ if __name__ == "__main__":
                 print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
             else:
                 print("Unknown operator. Available operators: +, -, * and /")
-                exit(1)
-            exit(0)
+                break
+            quit(1)
