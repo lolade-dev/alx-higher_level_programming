6a070eccdf4f52a230ab1f643f6dcabc0e65257a
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-07-06 08:05:30 +0100

    Update 4-hidden_discovery.py

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    for str in range(len(dir(hidden_4))):
-        if dir(hidden_4)[str][:2] != "__":
-            print("{:s}".format(dir(hidden_4)[str]))
+    for i in dir(hidden_4):
+        if not (i[0] == '_' and i[1] == '_'):
+            print(i)
