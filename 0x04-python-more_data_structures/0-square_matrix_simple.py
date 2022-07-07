266dde47bc300a6214f56e5b51d6b68007947050
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-07-07 04:58:56 +0100

    sq matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    return [list(map((lambda x: x * x), elm)) for elm in matrix]
+    new_matrix = []
+    for i in matrix:
+        tmp_list = list(map(lambda x: x * x, i))
+        new_matrix.append(tmp_list)
+    return (new_matrix)
