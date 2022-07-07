dedcf9cfe26409cd5141a864a4fd1457170f3219
Ololade Olanrewaju <49099449+lolade-dev@users.noreply.github.com>
2022-07-07 04:54:04 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    def s_r_elm(elm):
-        return (elm if elm != search else replace)
-    return list(map(s_r_elm, my_list))
+    new_list = my_list.copy()
+    for idx in range(len(my_list)):
+        if (my_list[idx] == search):
+            new_list[idx] = replace
+    return (new_list)
