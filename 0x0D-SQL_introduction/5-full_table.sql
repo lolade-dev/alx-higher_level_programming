2b27c4cb1272aada3204a531e5557c6ff1ec17ea
Ololade Olanrewaju <Oolarenwaju@wragbysolutions.com>
2022-09-07 08:52:40 +0100

    Corrections

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -2,4 +2,4 @@
 -- 'first_table' should have (id INT) and (name VARCHAR(256))
 -- db name will be passed as argument of mysql cmd
 -- If table 'first_table' already exists, should not fail.
-CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
+SHOW CREATE TABLE `first_table`;
